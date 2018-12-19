b08b58ce19061fd3c110e38518fa31631d6e06a3
guillaume autret <guillaume.autret@orange.fr>
2018-12-19 16:58:48 +0100

    ajax aside

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ const responsiveNav = require('responsive-nav');
 require('responsive-nav/client/dist/styles/responsive-nav.css');
 
 const nav = responsiveNav('nav');
+
+if (window.matchMedia("(min-width: 35em)").matches) {
+  console.log('bonjour');
+}
