70a3d5477436ae7e84fc7d017547643d7fe18129
guillaume autret <guillaume.autret@orange.fr>
2018-12-20 17:24:18 +0100

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,16 @@ module.exports = {
           'postcss-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
